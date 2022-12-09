ad1da90a8f9d9d19be3211116fa133627808d87f
Joseph H <vanhayes618@gmail.com>
2022-12-08 20:00:12 -0600

    some changes, i forget exactly what

---
+++ Aero design/Flight sim/PitchSim.m
@@ -0,0 +1,10 @@
+%% Pitch simulation - Crash-sat
+clear all
+close all
+clc
+
+%% Assumptions
+% - Crash sat is nearly vertical when beginning pitch
+% - Any wind, is constant
+% - Crash sat performs a constant G turn until horizontal with the ground
+
